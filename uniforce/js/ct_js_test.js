cd3fb62bef6367786a7fff160088d109ca2429b0
Viktor <Bazz@bk.ru>
2020-02-03 21:17:22 +0300

    Fix: BFP hidden field fixed.

--- uniforce/js/ct_js_test.js
+++ uniforce/js/ct_js_test.js
@@ -5,27 +5,30 @@ function ctSetCookie(c_name, value) {
 }
 
 var ctStart = function(){
+
 	ctSetCookie("spbct_checkjs", spbct_checkjs_val);
 	ctSetCookie("spbct_timezone", ct_date.getTimezoneOffset()/60*(-1));
 	ctSetCookie("spbct_ps_timestamp", Math.floor(new Date().getTime()/1000));
+
+	for( var i = 0 ; i < document.forms.length; i++ ){
+		var form = document.forms[i];
+		for( var e = 0; e < form.elements.length; e++ ) {
+			var element = form.elements[e];
+			if( element.type === 'password' ) {
+				var ct_input = document.createElement("input");
+				ct_input.name  = 'spbct_login_form';
+				ct_input.type  = 'hidden';
+				ct_input.value = '';
+				form.appendChild(ct_input);
+				break;
+			}
+		}
+	}
+
 };
 
 if(typeof window.addEventListener == "function"){
 	document.addEventListener("DOMContentLoaded", ctStart);
 }else{
 	document.attachEvent("DOMContentLoaded", ctStart);
-}
-
-for( var i = 0 ; i < document.forms.length; i++ ){
-	var form = document.forms[i];
-	for( var e = 0; document.forms[i].elements; e++ ) {
-		if( document.forms[i].elements[e].type === 'password' ) {
-			var ct_input = document.createElement("input");
-			ct_input.name  = 'spbct_login_form';
-			ct_input.type  = 'hidden';
-			ct_input.value = '';
-			document.forms[i].appendChild(ct_input);
-			break;
-		}
-	}
 }
