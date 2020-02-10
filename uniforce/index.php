79e8f1e26c163e136b1d9a9260a2d5d1dc14627c
Roman Safronov <r.safronik@gmail.com>
2020-02-10 12:18:43 +0300

    Fix: Independent PHP version check.

--- uniforce/index.php
+++ uniforce/index.php
@@ -1,12 +1,23 @@
 <?php
 
-    // Check requirements
+    // Check PHP requirements
     if( version_compare( phpversion(), '5.6', '<' ) ){ ?>
-
-        <h4 class="text-center">PHP version is <?php echo phpversion(); ?></h4>
-        <h4 class="text-center">Universal Security Plugin by CleanTalk requires version 5.6 or higher.</h4>
-        <h4 class="text-center">Please, contact your hosting provider to update it.</h4>
-
+        <div style="text-align: center">
+            <h4>PHP version is <?php echo phpversion(); ?></h4>
+            <h4>Universal Security Plugin by CleanTalk requires version 5.6 or higher.</h4>
+            <h4>Please, contact your hosting provider to update it.</h4>
+        </div>
+<?php
+    // Check Wordpress installation
+    }elseif( file_exists(__DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'index.php') &&
+        stripos( file_get_contents(__DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'index.php' ), '@package wordpress') !== false
+    ){
+?>
+        <div style="text-align: center">
+            <h4></h4>
+            <h4>Universal Security Plugin by CleanTalk can not be installed on WordPress.</h4>
+            <h4>Please, use <a href="https://wordpress.org/plugins/security-malware-firewall/">plugin</a> from Wordpress catalog.</h4>
+        </div>
 <?php
     }else{
         header('Location: router.php');
