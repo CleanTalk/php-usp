f47ecc6e6aec6d1f80463115402a3d2c4be16516
Roman Safronov <r.safronik@gmail.com>
2020-02-19 17:35:42 +0300

    Fix: Output for Summary tab.

--- uniforce/inc/settings.php
+++ uniforce/inc/settings.php
@@ -3,34 +3,40 @@
 use Cleantalk\Common\State;
 
 function ctusp_settings__show_cms(){
-	return '<p>Presumably CMS:' . State::getInstance()->data->detected_cms . '</p>';
+	echo '<p>Presumably CMS:' . State::getInstance()->data->detected_cms . '</p>';
 }
 
 function ctusp_settings__show_modified_files(){
-//	var_dump( State::getInstance()->data->modified_files->convertToArray );
-	$out = '<p>Modified files:</p>';
+	echo '<p>Modified files:</p>';
 	foreach( State::getInstance()->data->modified_files->convertToArray() as $file ){
-		$out .= '<p>&nbsp; - ' . $file . '</p>';
+		echo '<p>&nbsp; - ' . $file . '</p>';
 	}
-	return $out;
 }
 
 function usp_settings__show_fw_statistics( $out = '' )
 {
 	$usp = State::getInstance();
-	$stat = State::getInstance()->data->stats;
+	$stat = State::getInstance()->data->stat;
 
 	if( $usp->settings->fw || $usp->settings->waf ) {
-		$out .= 'Security FireWall was updated: ' . ( $stat->fw->last_update ? date('M d Y H:i:s', $stat->fw->last_update) : 'never' ) . '<br>';
-		$out .= 'Security FireWall contains: ' . $stat->fw->entries . ' entires.<br>';
-		$out .= 'Security FireWall logs were sent: ' . $stat->fw->logs_sent_time ? date('M d Y H:i:s', $stat->fw->logs_sent_time) : 'never.' . '<br>';
-		$out .= '<br>';
+		echo 'Security FireWall was updated: ' . ( $stat->fw->last_update
+				? date('M d Y H:i:s', $stat->fw->last_update)
+				: 'never'
+			) . '<br>';
+		echo 'Security FireWall contains: ' . ( $stat->fw->entries ? $stat->fw->entries : 'no' ). ' entires.<br>';
+		echo 'Security FireWall logs were sent: ' . ( $stat->fw->logs_sent_time
+				? date('M d Y H:i:s', $stat->fw->logs_sent_time)
+				: 'never.'
+			) . '<br>';
+		echo '<br>';
 	}
-	if( $usp->settings->bf ) {
-		$bfp_send_logs_time = $uniforce_bfp_last_logs_send ? date('M d Y H:i:s', $uniforce_bfp_last_logs_send) : 'never.';
-		$out .= 'BruteForce Protection was triggered: ' . $stat->bf->logs_sent_amount . '<br>';
-		$out .= 'BruteForce Protection logs were sent: ' . $stat->bf->logs_sent_time . '<br>';
-		$out .= '<br>';
+	if( $usp->settings->bfp ) {
+		$bfp_send_logs_time = $stat->bfp->logs_sent_time ?  : 'never.';
+		echo 'BruteForce Protection was triggered: ' . $stat->bfp->count . ' times.<br>';
+		echo 'BruteForce Protection logs were sent: ' . ( $stat->bfp->logs_sent_time
+				? date( 'M d Y H:i:s', $stat->bfp->logs_sent_time )
+				: 'never'
+			). '<br>';
+		echo '<br>';
 	}
-	return $out;
 }
