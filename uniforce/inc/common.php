3c8a07011f59d16899a019448a5971d2623c7121
Roman Safronov <r.safronik@gmail.com>
2020-03-12 15:13:41 +0500

    Version: 3.0.

--- uniforce/inc/common.php
+++ uniforce/inc/common.php
@@ -9,7 +9,7 @@
 use Cleantalk\Variables\Server;
 
 if( ! defined( 'SPBCT_PLUGIN' ) )     define( 'SPBCT_PLUGIN', 'uniforce' );
-if( ! defined( 'SPBCT_VERSION' ) )    define( 'SPBCT_VERSION', '1.0' );
+if( ! defined( 'SPBCT_VERSION' ) )    define( 'SPBCT_VERSION', '3.0' );
 if( ! defined( 'SPBCT_AGENT' ) )      define( 'SPBCT_AGENT', SPBCT_PLUGIN . '-' . str_replace( '.', '', SPBCT_VERSION ) );
 if( ! defined( 'SPBCT_USER_AGENT' ) ) define( 'SPBCT_USER_AGENT', 'Cleantalk-Security-Universal-Plugin/' . SPBCT_VERSION );
 
