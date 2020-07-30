09f49a1593372b54e23b63a964a16ac5047a04f7
Glomberg <bazz@bk.ru>
2020-07-30 15:29:16 +0300

    Fix: Error class fixed.

--- uniforce/lib/Cleantalk/Common/Err.php
+++ uniforce/lib/Cleantalk/Common/Err.php
@@ -11,22 +11,22 @@ namespace Cleantalk\Common;
  */
 class Err{
 	
-	static $instance;
-	public $errors = [];
+	private static $instance;
+	private $errors = [];
 	
-	public function __construct(){}
-	public function __wakeup(){}
-	public function __clone(){}
+	private function __construct(){}
+    private function __wakeup(){}
+    private function __clone(){}
 	
 	/**
 	 * Constructor
 	 */
 	public static function getInstance(){
-		if (!isset(static::$instance)) {
-			static::$instance = new static;
-			static::$instance->init();
+		if (!isset(self::$instance)) {
+            self::$instance = new self;
+            self::$instance->init();
 		}
-		return static::$instance;
+		return self::$instance;
 	}
 	
 	/**
@@ -63,7 +63,7 @@ class Err{
 		return $out;
 	}
 	
-	public function get_all( $output_style = 'string' ){
+	public static function get_all( $output_style = 'string' ){
 		$out = self::$instance->errors;
 		if($output_style == 'as_json')
 			$out = json_encode( self::$instance->errors, true );
@@ -76,7 +76,7 @@ class Err{
 	
 	public static function check_and_output( $output_style = 'string' ){
 		if(self::check())
-			return self::$instance->get_last( $output_style );
+			return self::get_last( $output_style );
 		else
 			return false;
 	}
