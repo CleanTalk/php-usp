0ae625a2ed15ac37e2b528b540c159a3fc10b914
safronik <Githubishe351@$^>
2020-11-13 21:22:38 +0300

    Fix: Database class for PHP lower then 7.0.

--- uniforce/lib/Cleantalk/USP/DB.php
+++ uniforce/lib/Cleantalk/USP/DB.php
@@ -34,7 +34,6 @@ class DB extends \PDO implements \Cleantalk\USP\Common\DB {
 			$options  = $params[3] ? $params[3] : array(
 				\PDO::ATTR_ERRMODE            => \PDO::ERRMODE_EXCEPTION, // Handle errors as an exceptions
 				\PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,        // Set default fetch mode as associative array
-				\PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => true,
 				\PDO::MYSQL_ATTR_SSL_CA => CT_USP_DATA_SSL_CERT . 'ca.pem',
 				\PDO::MYSQL_ATTR_SSL_CERT => CT_USP_DATA_SSL_CERT . 'client-cert.pem',
 				\PDO::MYSQL_ATTR_SSL_KEY => CT_USP_DATA_SSL_CERT . 'client-key.pem',
