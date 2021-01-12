bb40b09ac4b6b3a746cae1dd9ed969508ca9f6b9
safronik <Githubishe351@$^>
2021-01-12 12:42:52 +0500

    Fix: phpBB possible autoloader conflict.

--- uniforce/lib/autoloader.php
+++ uniforce/lib/autoloader.php
@@ -13,7 +13,7 @@ function ct_usp_autoloader( $class ){
 	
 	// Register class auto loader
 	// Custom modules
-	if( strpos( $class, 'Cleantalk' ) !== false ){
+	if( strpos( $class, 'Cleantalk\\USP' ) !== false ){
 		$class = str_replace( '\\', DIRECTORY_SEPARATOR, $class );
 		$class_file = __DIR__ . DIRECTORY_SEPARATOR . $class . '.php';
 		if( file_exists( $class_file ) ){
