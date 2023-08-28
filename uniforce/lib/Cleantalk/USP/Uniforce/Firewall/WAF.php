6d0b80b5caf2893a83cd7e72721f3bdab3ee6a63
alexandergull <galyshev@cleantalk.org>
2023-08-28 22:53:36 +0500

    WAF.php. Tabs to spaces.

--- uniforce/lib/Cleantalk/USP/Uniforce/Firewall/WAF.php
+++ uniforce/lib/Cleantalk/USP/Uniforce/Firewall/WAF.php
@@ -9,26 +9,26 @@ use Cleantalk\USP\Variables\Server;
 use Cleantalk\USP\Scanner\ScannerH;
 
 class WAF extends \Cleantalk\USP\Uniforce\Firewall\FirewallModule {
-	
+
 	public $module_name = 'WAF';
-	
+
 	protected $waf_xss_check     = false;
 	protected $waf_sql_check     = false;
 	protected $waf_file_check    = false;
 	protected $waf_exploit_check = false;
-	
+
 	private $waf_pattern       = array(); // Why WAF is triggered (reason)
-	
+
 	private $waf_sql_patterns = array();
 	private $waf_exploit_patterns = array();
 	private $waf_xss_patterns = array();
-	
+
 	public $waf_file_mime_check = array(
 		'text/x-php',
 		'text/plain',
 		'image/x-icon',
 	);
-	
+
 	/**
 	 * FireWall_module constructor.
 	 * Use this method to prepare any data for the module working.
@@ -36,28 +36,28 @@ class WAF extends \Cleantalk\USP\Uniforce\Firewall\FirewallModule {
 	 * @param array $params
 	 */
 	public function __construct( $params = array() ){
-		
+
 		parent::__construct( $params );
-		
+
 	}
-	
+
 	/**
 	 * Use this method to execute main logic of the module.
 	 * @return mixed
 	 */
 	public function check() {
-		
+
 		$results = array();
-		
+
 		// Get signatures
 		$signatures = $this->signatures__get();
-		
+
 		if ( $signatures ) {
-			
+
 			foreach ( $signatures as $signature ) {
 				
 				switch ( $signature['attack_type'] ) {
-					
+
 					case 'SQL_INJECTION':
 						$this->waf_sql_patterns[] = $signature['body'];
 						break;
@@ -70,38 +70,38 @@ class WAF extends \Cleantalk\USP\Uniforce\Firewall\FirewallModule {
 				}
 			}
 		}
-		
+
 		// XSS
 		if( $this->waf_xss_check ){
 			if($this->waf_xss_check($_POST) || $this->waf_xss_check($_GET) || $this->waf_xss_check($_COOKIE)){
 				$results[] = array('ip' => end($this->ip_array), 'is_personal' => false, 'status' => 'DENY_BY_WAF_XSS', 'pattern' => $this->waf_pattern);
 			}
 		}
-		
+
 		// SQL-injection
 		if( $this->waf_sql_check ){
 			if($this->waf_sql_check($_POST) || $this->waf_sql_check($_GET)){
 				$results[] = array('ip' => end($this->ip_array), 'is_personal' => false, 'status' => 'DENY_BY_WAF_SQL', 'pattern' => $this->waf_pattern);
 			}
 		}
-		
+
 		// File
 		if ($this->waf_file_check ){
 			if($this->waf_file_check()){
 				$results[] = array('ip' => end($this->ip_array), 'is_personal' => false, 'status' => 'DENY_BY_WAF_FILE', 'pattern' => $this->waf_pattern);
 			}
 		}
-		
+
 		// Exploits
 		if( $this->waf_exploit_check ){
 			if($this->waf_exploit_check()){
 				$results[] = array('ip' => end($this->ip_array), 'is_personal' => false, 'status' => 'DENY_BY_WAF_EXPLOIT', 'pattern' => $this->waf_pattern);
 			}
 		}
-		
+
 		if( ! $results )
 			$results[] = array( 'status' => 'PASS' );
-		
+
 		foreach( $results as &$result ){
 			$result = array_merge(
 				$result,
@@ -112,9 +112,9 @@ class WAF extends \Cleantalk\USP\Uniforce\Firewall\FirewallModule {
 				)
 			);
 		}
-		
+
 		return $results;
-		
+
 	}
 
     /**
@@ -122,7 +122,7 @@ class WAF extends \Cleantalk\USP\Uniforce\Firewall\FirewallModule {
      * @return array|false
      */
     private function signatures__get(){
-		
+
 		$signatures_source = new Storage('signatures', null, '', 'csv', array(
 			'id',
 			'name',
@@ -142,7 +142,7 @@ class WAF extends \Cleantalk\USP\Uniforce\Firewall\FirewallModule {
         }
         return !empty($signatures) ? $signatures : false;
 	}
-	
+
 	/**
 	 * Checks array for XSS-attack patterns
 	 *
@@ -151,9 +151,9 @@ class WAF extends \Cleantalk\USP\Uniforce\Firewall\FirewallModule {
 	 * @return bool
 	 */
 	private function waf_xss_check( $arr ) {
-		
+
 		foreach( $arr as $name => $param ){
-			
+
 			// Recursion
 			if( is_array( $param ) ){
 				$result = $this->waf_xss_check( $param );
@@ -161,7 +161,7 @@ class WAF extends \Cleantalk\USP\Uniforce\Firewall\FirewallModule {
 					return true;
 				continue;
 			}
-			
+
 			//Check
 			foreach( $this->waf_xss_patterns as $pattern ){
                 $is_regexp = preg_match( '@^/.*/$@', $pattern ) || preg_match( '@^#.*#$@', $pattern );
@@ -175,11 +175,11 @@ class WAF extends \Cleantalk\USP\Uniforce\Firewall\FirewallModule {
                 }
 			}
 		}
-		
+
 		return false;
-		
+
 	}
-	
+
 	/**
 	 * Checks array for SQL injections
 	 *
@@ -188,16 +188,16 @@ class WAF extends \Cleantalk\USP\Uniforce\Firewall\FirewallModule {
 	 * @return bool
 	 */
 	private function waf_sql_check( $arr ) {
-		
+
 		foreach( $arr as $name => $param ){
-			
+
 			if( is_array( $param ) ){
 				$result = $this->waf_sql_check( $param );
 				if( $result === true )
 					return true;
 				continue;
 			}
-			
+
 			foreach( $this->waf_sql_patterns as $pattern ){
                 $is_regexp = preg_match( '@^/.*/$@', $pattern ) || preg_match( '@^#.*#$@', $pattern );
 
@@ -210,18 +210,18 @@ class WAF extends \Cleantalk\USP\Uniforce\Firewall\FirewallModule {
                 }
 			}
 		}
-		
+
 		return false;
-		
+
 	}
-	
+
 	/**
 	 * Checks $_SERVER['QUERY_STRING'] for exploits
 	 *
 	 * @return bool
 	 */
 	private function waf_exploit_check() {
-		
+
 		foreach( $this->waf_exploit_patterns as $pattern ){
             $is_regexp = preg_match( '@^/.*/$@', $pattern ) || preg_match( '@^#.*#$@', $pattern );
 
@@ -233,18 +233,18 @@ class WAF extends \Cleantalk\USP\Uniforce\Firewall\FirewallModule {
                 return true;
             }
 		}
-		
+
 		return false;
-		
+
 	}
-	
+
 	/**
 	 * Checks uploaded files for malicious code
 	 *
 	 * @return boolean Does the file contain malicious code
 	 */
 	private function waf_file_check() {
-		
+
 		if( ! empty( $_FILES ) ){
 			foreach( $_FILES as $filez ){
 				if ( ( empty($filez['errror'] ) || $filez['errror'] == UPLOAD_ERR_OK ) ) {
@@ -271,8 +271,8 @@ class WAF extends \Cleantalk\USP\Uniforce\Firewall\FirewallModule {
 				}
 			}
 		}
-		
+
 		return false;
-		
+
 	}
 }
