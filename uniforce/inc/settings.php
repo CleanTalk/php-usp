062a376063da2f63d866d1a1717e182ef791bca4
alexandergull <galyshev@cleantalk.org>
2023-09-01 13:20:26 +0500

    Fix. Call the magic method before empty check.

--- uniforce/inc/settings.php
+++ uniforce/inc/settings.php
@@ -16,7 +16,8 @@ function ctusp_settings__show_modified_files(){
 function usp_settings__plugin_state()
 {
     $usp = State::getInstance();
-    if ( ! empty($usp->plugin_meta->latest_version) ) {
+    $usp->plugin_meta->latest_version;
+    if (! empty($usp->plugin_meta->latest_version) ) {
         if ( version_compare($usp->plugin_meta->version, $usp->plugin_meta->latest_version) === -1 ) {
             echo '<p class="text-center">There is a newer version. Update to the latest ' . $usp->plugin_meta->latest_version . '</p>';
             echo '<p class="text-center"><button id="btn-update" form="none" class="btn btn-setup" value="">Update</button><img class="preloader" src="img/preloader.gif"></p>';
