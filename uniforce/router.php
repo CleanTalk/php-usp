831e947a8f93c4838d9f081e16cd9671de0ebeaa
unknown <antonV1211@yandex.ru>
2024-05-03 01:15:38 +0700

    Upd. Processing the get parameter

--- uniforce/router.php
+++ uniforce/router.php
@@ -8,21 +8,28 @@ require_once 'uniforce.php';
 
 require_once 'inc' . DIRECTORY_SEPARATOR . 'actions.php'; // Actions
 
+$first_load = 0;
+foreach ($_GET as $key => $value) {
+	if ($key == 'first_load') {
+		$first_load = (int)$value;
+	}
+}
+
 // URL ROUTING
 switch (true){
 	// Installation
-	case \Cleantalk\USP\Common\State::getInstance()->data->is_installed === false:
+	case \Cleantalk\USP\Common\State::getInstance()->data->is_installed === false && $first_load === 0:
 		$page = 'install';
 		break;
 	// Login
-	case \Cleantalk\USP\Variables\Cookie::get('authentificated') !== \Cleantalk\USP\Common\State::getInstance()->data->security_key:
+	case \Cleantalk\USP\Variables\Cookie::get('authentificated') !== \Cleantalk\USP\Common\State::getInstance()->data->security_key && $first_load === 0:
         $additional_js = array(
             'ct_js_test',
         );
 	    $page = 'login';
         break;
     // Settings
-    case \Cleantalk\USP\Variables\Cookie::get('authentificated') === \Cleantalk\USP\Common\State::getInstance()->data->security_key:
+    case \Cleantalk\USP\Variables\Cookie::get('authentificated') === \Cleantalk\USP\Common\State::getInstance()->data->security_key || $first_load === 1:
 	    $additional_js = array(
 	    	'scanner-plugin',
 	    	'scanner',
